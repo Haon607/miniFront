ab668b0b48db37ae80e21352cda3745a660de5eb
Noah Hamsa <noah.hamsa@twinformatics.at>
2024-10-29 16:01:55 +0100

    savestate

--- src/app/game/rules.game/rules.game.component.ts
+++ src/app/game/rules.game/rules.game.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
-import {SquaresService} from "../../squares/squares.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {MemoryGameService} from "../../service/memory/memory.game.service";
-import {GameReqService} from "../../service/request/game.req.service";
+import { Component } from '@angular/core';
+import { SquaresService } from "../../squares/squares.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MemoryGameService } from "../../service/memory/memory.game.service";
+import { GameReqService } from "../../service/request/game.req.service";
+import { Game } from "../../models";
 
 @Component({
   selector: 'app-rules.game',
@@ -15,6 +16,7 @@ export class RulesGameComponent {
   displayHints: boolean = false;
   rulesMusic = new Audio();
   roundNumber = '';
+  game: Game = new Game();
 
   constructor(
     private squares: SquaresService,
@@ -25,25 +27,27 @@ export class RulesGameComponent {
   ) {
     this.roundNumber = activatedRoute.snapshot.paramMap.get('round')!;
 
-    gameService.modifyData(memory.gameId, '/rules/' + this.roundNumber).subscribe(() => {});
+    gameService.modifyData(memory.gameId, '/rules/' + this.roundNumber).subscribe(game =>
+      this.game = game
+    );
     this.displayHints = false;
 
-    this.rulesMusic.src = '/audio/rules.mp3';
+    this.rulesMusic.src = '/audio/rules.mp3'; //TODO
     this.rulesMusic.play();
     this.startAnimation();
-    this.rulesMusic.addEventListener('ended', () => {
-      // this.skipToNext();
-    });
-
-  }
-
-  private async startAnimation() {
-
   }
 
   skipToNext() {
     this.rulesMusic.pause();
     this.rulesMusic.currentTime = 0;
     this.router.navigateByUrl(`/game/round/${this.roundNumber}`)
+  }
+
+  private async startAnimation() {
+    //TODO
+  }
+
+  get roundName() {
+    return this.game.rounds[Number(this.roundNumber)].name;
   }
 }
